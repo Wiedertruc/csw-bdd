26841d83e64c8d1e467e46386fe68581dda2ca8a
Wiederhold Camille <camille.wiederhold@etudiant.univ-rennes1.fr>
2020-12-16 18:55:31 +0100

    Update exo1TP2.php ajoute comm pouet

--- TP2/exo1TP2.php
+++ TP2/exo1TP2.php
@@ -2,6 +2,6 @@
 
 echo 'Bonjour '.$_GET['prenom'].' <b>'.$_GET['nom'].'</b>. <br/>
 	Nous avons bien noté que vous habitez <br/>'.$_GET['adresse'].' à <b>'.$_GET['ville'].'</b> ('.$_GET['codePostal'].').';
-
+//pouet
 ?>
 <br><a href="principal.php">back</a>
