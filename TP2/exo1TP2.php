5103dc0788ca48b5726853b1acb4e6723166e5ef
Camille Wiederhold <camille.wiederhold@gmail.com>
2020-12-16 19:03:11 +0100

    ajout cacahuète et prout

--- TP2/exo1TP2.php
+++ TP2/exo1TP2.php
@@ -1,7 +1,8 @@
 <?php 
-
+//prout
 echo 'Bonjour '.$_GET['prenom'].' <b>'.$_GET['nom'].'</b>. <br/>
 	Nous avons bien noté que vous habitez <br/>'.$_GET['adresse'].' à <b>'.$_GET['ville'].'</b> ('.$_GET['codePostal'].').';
 //pouet
+//cacahuète
 ?>
 <br><a href="principal.php">back</a>
